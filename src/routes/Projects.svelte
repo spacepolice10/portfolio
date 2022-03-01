fa29e7e19f80d662683a23df3933c5d3a440110d
spacepolice10 <44314842+spacepolice10@users.noreply.github.com>
2022-03-01 15:21:09 +0300

    fix: remove logs

--- src/routes/Projects.svelte
+++ src/routes/Projects.svelte
@@ -1,7 +1,6 @@
 <script>
   import Project from "../components/Project.svelte";
   import data from "./data";
-  console.log(data.projects);
 </script>
 
 <div class="grid grid-cols-1 lg:grid-cols-3 lg:h-screen">
